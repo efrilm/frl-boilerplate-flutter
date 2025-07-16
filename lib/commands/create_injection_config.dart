463121f23133d6f96af1d7515c0d9ed22b45f8dd
efrilm <efril.mulanda@gmail.com>
2025-07-17 00:02:23 +0700

    feat: create injection

---
+++ lib/commands/create_injection_config.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+import 'package:frl_boilerplate/utils/logger.dart';
+
+void createInjection() {
+  Logger.section('Generating injection.dart...');
+
+  final path = 'lib';
+  final filePath = '$path/injection.dart';
+  final file = File(filePath);
+
+  if (!Directory(path).existsSync()) {
+    Directory(path).createSync(recursive: true);
+    Logger.success('Created folder: $path');
+  }
+
+  if (file.existsSync()) {
+    Logger.warning('⏭️ injection.dart already exists. Skipped generating.');
+    return;
+  }
+
+  final content = '''
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+import 'injection.config.dart';
+
+final getIt = GetIt.instance;
+
+@InjectableInit()
+Future<void> configureDependencies(String env) => getIt.init(environment: env);
+''';
+
+  file.writeAsStringSync(content);
+  Logger.success('injection.dart generated at: $filePath');
+}
--- lib/commands/init_project_command.dart
+++ lib/commands/init_project_command.dart
@@ -2,6 +2,7 @@ import 'package:frl_boilerplate/commands/add_dependecy_command.dart';
 import 'package:frl_boilerplate/commands/create_assets_command.dart';
 import 'package:frl_boilerplate/commands/create_common_command.dart';
 import 'package:frl_boilerplate/commands/create_env_command.dart';
+import 'package:frl_boilerplate/commands/create_injection_config.dart';
 import 'package:frl_boilerplate/utils/logger.dart';
 
 Future<void> initProject() async {
@@ -11,6 +12,7 @@ Future<void> initProject() async {
   createAssetFolders();
   createCommon();
   createEnv();
+  createInjection();
 
   Logger.section('✅ frl_boilerplate init completed!');
 }
