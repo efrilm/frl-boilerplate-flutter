41d1ca939461c9ffb1cfb61a12f08cfe1a802b30
efrilm <efril.mulanda@gmail.com>
2025-07-16 23:45:56 +0700

    feat: common folder

---
+++ lib/commands/create_common_command.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:frl_boilerplate/utils/logger.dart';
+
+void createCommon() {
+  Logger.section('Create common folder...');
+  final baseDir = Directory('lib/common');
+  baseDir.createSync(recursive: true);
+
+  // Subfolders
+  final subfolders = [
+    'api',
+    'constants',
+    'di',
+    'extension',
+    'formatter',
+    'function',
+    'network',
+    'resources',
+    'url',
+  ];
+  for (var folder in subfolders) {
+    Directory('${baseDir.path}/$folder').createSync(recursive: true);
+    Logger.success('Created folder: $folder');
+  }
+
+  Logger.success('Create common folder Success');
+}
--- lib/commands/init_project_command.dart
+++ lib/commands/init_project_command.dart
@@ -1,12 +1,14 @@
 import 'package:frl_boilerplate/commands/add_dependecy_command.dart';
 import 'package:frl_boilerplate/commands/create_assets_command.dart';
+import 'package:frl_boilerplate/commands/create_common_command.dart';
 import 'package:frl_boilerplate/utils/logger.dart';
 
 Future<void> initProject() async {
   Logger.section('🔧 Running frl_boilerplate init...');
 
-  await addDependencies();
-  createAssetFolders();
+  // await addDependencies();
+  // createAssetFolders();
+  createCommon();
 
   Logger.section('✅ frl_boilerplate init completed!');
 }
